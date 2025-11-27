5e8bb9fa5ac893935942cd1541a56f7d771dfae6
Gunay Shuaib <gunayyshuaib@gmail.com>
2025-11-27 15:28:16 +0200

    app.py got fixed

--- app.py
+++ app.py
@@ -13,16 +13,16 @@ def index():
 
 @app.route('/metrics')
 def metrics():
-# simple example metrics — adapt to your needs
-cpu = psutil.cpu_percent(interval=0.2)
-mem = psutil.virtual_memory().percent
-now = int(time.time() * 1000)
-data = {
-'timestamp': now,
-'cpu': cpu,
-'memory': mem
-}
-return jsonify(data)
+	# simple example metrics — adapt to your needs
+	cpu = psutil.cpu_percent(interval=0.2)
+	mem = psutil.virtual_memory().percent
+	now = int(time.time() * 1000)
+	data = {
+		'timestamp': now,
+		'cpu': cpu,
+	'memory': mem
+	}
+	return jsonify(data)
 
 
 if __name__ == '__main__':
